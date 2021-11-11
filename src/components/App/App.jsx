6704932df981913f59c25550fa88f8560630324b
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-11-11 09:40:38 -0600

    added test component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import Testerooni from '../Testerooni/Testerooni'
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+      <Testerooni/>
     </div>
   );
 }
