24a980cdfde9515c62936ea19a40977155a7e52f
Jennifer Alexander <jenny_alexander@icloud.com>
2021-11-11 09:43:18 -0600

    Add little test feature

---
+++ src/components/JenPizza/JenPizza.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+
+function JenPizza() {
+
+    return (
+      <div>
+        <p>Jen's pizza component is up!!!</p>
+      </div>
+    );
+  }
+  
+  export default JenPizza;
