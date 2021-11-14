529e59e3ec51ad20c0a2884dcf5ac0d24ba21516
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-11-13 20:00:41 -0600

    updated favicon

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import Admin from '../Admin/Admin'; //todo remove this before merging branch
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
@@ -10,11 +11,12 @@ function App() {
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-      <Admin/>
-      {/* todo ^ remove this before merging branch */}
+      
+      <BrowserRouter>
+        <Routes>
+            <Route path='/Admin' element={<Admin/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
