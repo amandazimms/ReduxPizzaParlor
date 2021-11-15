535faa0615c87168c60a8d08602869c944ad7051
Miriam McNamara <miriammcnamara@miriamscomputer.lan>
2021-11-15 13:36:04 -0600

    confirmation dialog skeleton added

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 // import {useDispatch, useSelector} from 'react-redux';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Typography, Grid, Container, Box, Button, Table, TableHead, TableBody, TableCell, TableContainer,
 TableRow, Paper } from '@material-ui/core';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 
 function Checkout( props ){
 
-    function createData(name, cost) {
+    function createData(name, cost) { 
         return { name, cost };
       }
       
       const rows = [
-        createData('Onamonapizza', 14.99),
-        createData('Pepperoni', 15.99),
+        createData('Onamonapizza', 14.99), //store.pizza.name and cost will be used here instead
+        createData('Pepperoni', 15.99), //do i need a listItem component if I don't need any functionality in my table?
       ];
     // const[ name, setName ]=useState( null );
 
@@ -20,70 +22,110 @@ function Checkout( props ){
 
     // const dispatch = useDispatch();
 
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleCloseDisagree = () => {
+      setOpen(false);
+    };
+
+    const handleCloseAgree = () => {
+      console.log( 'inhandleCloseAgree' );
+          //so if the user selects OK, it sends the POST dispatch with no payload AND routes the
+      //user back to the homepage, which I'm not entirely sure how to
+      //format in a function as apposed to a link on the DOM
+    }
+
+
     return(
-        <Container>
-            <Typography variant="h2">Step 3: Checkout</Typography>
-          <Grid container component={Paper}>
-            <Grid item>
-              <Box px={10} py={5}>
-                <Paper>
-                  <Box p={3}>
-                  <Typography variant="h4">order.name</Typography>
-                  <Typography variant="h4">order.street_address</Typography>
-                  <Typography variant="h4">order.city, order.zip</Typography>
-                  </Box>
-                </Paper>
-              </Box>
-            </Grid>
-            <Grid>
-            <Box px={10} py={5} >
-              </Box>
-            </Grid>
-            <Grid>
-            <Box px={10} py={5} >
-              </Box>
-            </Grid>
-            <Grid item>
-              <Box px={10} py={5}>
-                <Paper item align="right">
-                <Box p={3}>
-                  <Typography variant="h4">order.type</Typography>
-                  </Box>
-                </Paper>
-              </Box>
-            </Grid>
+      <Container><Typography variant="h2">Step 3: Checkout</Typography>
+        <Grid container spacing={2} component={Paper}>
+
+          <Grid item xs={3}>
+            <Box p={3}>
+              <Paper>
+                <Typography variant="h5">order.name</Typography>
+                <Typography variant="h5">order.street_address</Typography>
+                <Typography variant="h5">order.city, order.zip</Typography>
+              </Paper>
+            </Box>
           </Grid>
+          <Grid item xs={6} />
+          <Grid item xs={3}>
+            <Box p={3}>
+              <Paper>
+                <Typography variant="h5">order.type</Typography>
+              </Paper>
+            </Box>
+          </Grid>
+
+          <Grid item xs={1} />
+          <Grid item xs={10}>
             <TableContainer component={Paper} >
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Cost</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow>
-              <TableCell>
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.cost}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <Box py={5}>
-      <Paper>
-        <Box p={2}>
-          <Typography align="right" variant="h4">Total: order.total</Typography>
-        </Box>
-      </Paper>
-    </Box>
-    <Button variant="contained" color="primary" size="large" style={{
-      fontSize: 24
-    }}>Checkout</Button>
-  </Container>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell><Typography variant="h6">Name</Typography></TableCell>
+                    <TableCell align="right"><Typography variant="h6">Cost</Typography></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow>
+                      <TableCell><Typography variant="subtitle1">{row.name}</Typography></TableCell>
+                      <TableCell align="right"><Typography variant="subtitle1">{row.cost}</Typography></TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item xs={1} />
+
+          <Grid item xs={8} />
+          <Grid item xs={4}>
+            <Box p={3}>
+              <Paper><Typography variant="h4">Total: order.total</Typography></Paper>
+            </Box>
+          </Grid>
+
+          <Grid item xs={4} />
+          <Grid item xs={4}>
+            <Box pb={5}>
+              <Button variant="contained" color="primary" size="large" style={{fontSize: 24}} onClick={handleClickOpen}>Checkout</Button>
+
+              <Dialog
+        open={open}
+        onClose={handleCloseAgree, handleCloseDisagree}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Are you sure you are ready to checkout?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            As soon as you click Agree, your order will be placed and hot, tasty pizza will
+            be on its way to you!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDisagree}>Disagree</Button>
+          <Button onClick={handleCloseAgree} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+            </Box>
+          </Grid>
+          <Grid item xs={4} />
+
+        </Grid>
+      </Container>
     )
 }
 
