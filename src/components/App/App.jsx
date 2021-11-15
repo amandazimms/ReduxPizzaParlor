597195b6cad767bf65adad532c724185a24b57c9
Jennifer Alexander <jenny_alexander@icloud.com>
2021-11-15 14:40:25 -0600

    Removed my test functionality so no conflict will happen when merging pull request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
 import axios from 'axios';
 import './App.css';
-import Header from '../Header/Header'
-import Customer from '../Customer/Customer';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
-  // we want to be able to dispatch to top of hioerarchy
-  const dispatch = useDispatch();
   return (
     <div className='App'>
-      {/* <header className='App-header'>
+      <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
-      </header> */}
-      {/* <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p> */}
-      <Customer />
+      </header>
+  
+      <img src='images/pizza_photo.png' />
+      <p>Pizza is great.</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,41 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import logger from 'redux-logger';
 
-// customer reducer
-const customer = ( state=[], action ) =>{
-    if( action.type === 'ADD_CUSTOMER' ) {
-      console.log( '------->in ADD_CUSTOMER' );
-      return [...state, action.payload ];
-    }
-    return state; //<------THIS IS VERY IMPORTANT. DON'T FORGET IT!!
-  }
-// pizza reducer
-const pizza = ( state= 35.98, action )=>{
-  if( action.type === 'GET_ORDER_TOTAL' ){
-    console.log( '------->GET_ORDER_TOTAL' );
-    return state;
-  }
-  return state;
-}
-// a store
-const storeInstance = createStore(
-    combineReducers(
-      {
-        customer,
-        pizza
-      }
-    ),
-    applyMiddleware(
-      logger
-    )
-  );
-
-ReactDOM.render(
-    <Provider store={storeInstance}>
-        <App />
-    </Provider>, 
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
