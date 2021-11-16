4ed4918fddaa847d65aaf8d2a8375a4507566ad9
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-11-16 13:43:37 -0600

    removed comments

--- src/index.js
+++ src/index.js
@@ -43,20 +43,7 @@ const orders =  (state = [], action) => {
         console.log( err );
         alert( 'problem!' );
     }) 
-}
-//todo can remove 'annabel' and 'josie' placeholder/testing data once Admin successfully displays DB orders on DOM
-// { name: 'Annabel', 
-// orderTime: '11/13 at 7:13pm', 
-// type: 'delivery', 
-// cost: 27.99, 
-// pizzas: [ 
-//     {id: 1, quantity: 1}, 
-//     {id: 5, quantity: 1} 
-// ] },
-// { name: 'Josie', orderTime: '11/13 at 6:29pm', type: 'pickup', cost: 21.99, pizzas: [ {id: 2, quantity: 1}, {id: 3, quantity: 1}, {id: 4, quantity: 1}  ] }
-
-// ], action) => {
-// return state;
+  }
   return state;
 }
 
