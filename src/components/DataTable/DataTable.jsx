55b0da87ab7c6c09a500eff87a7f15f8aa0406de
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-11-16 12:21:33 -0600

    working on adding dispatch to datatable. last commit before simplifying rows to orders

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -12,31 +12,62 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import {useSelector} from 'react-redux';
-
+import {useSelector, useDispatch} from 'react-redux';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 //DataTable is a sleek MUI collapsible table
 function DataTable() {
-  //on component load: 
-  //dispatch type: "GET_ORDERS"
-  //store the results in store.orders
-  
-    const orders = useSelector(store=> store.orders);
+    const dispatch = useDispatch();
+    const orders = useSelector( store=>store.orders );
+    let [rows, setRows]=useState([]);
 
-    const rows = orders;
-        //todo can remove 'annabel' and 'josie' placeholder/testing data once Admin successfully displays DB orders on DOM
-        // [{ 
-        //     name: 'Annabel', 
-        //     orderTime: '11/13 at 7:13pm', 
-        //     type: 'delivery', 
-        //     cost: 27.99, 
-        //     pizzas: [ 
-        //         {id: 1, quantity: 1}, 
-        //         {id: 5, quantity: 1} 
-        //     ] },
+    useEffect(()=>{
+      getOrders();
+      //setRows([{ name: 'Annabel', orderTime: '11/13 at 7:13pm', type: 'delivery', cost: 27.99, pizzas: [{id: 1, quantity: 1}, {id: 5, quantity: 1}]},{ name: 'Annabel', orderTime: '11/13 at 7:13pm', type: 'delivery', cost: 27.99, pizzas: [{id: 1, quantity: 1}, {id: 5, quantity: 1}]}] );
+      console.log('--------------->rows:', rows); 
+
+    const getOrders = () => {
+      axios.get('/api/order').then ( ( response )=>{
+        console.log('getOrders response.data:', response.data);
+        dispatch({type: 'SET_ORDERS', payload: response.data});
+        console.log('in dt after dispatch');
+        return formatOrders(response.data);
+      }).catch( ( err )=>{
+        console.log( err );
+        alert( 'problem!' );
+      }) 
+    } 
+
+    function formatOrders(orders){
+      let formattedOrders = [];
+
+      for(const order of orders){
+        formattedOrders.push({
+          name: order.customer_name,
+          orderTime: order.time,
+          type: order.type,
+          cost: '$'+order.total,
+          pizzas: [{id: 0, quantity: 0},{id: 0,quantity: 0}]//<-placeholders. change to findPizzasForOrder(order.id)
+        })
+      }
+      //setRows(orders);      
+      console.log('formattedOrders:', formattedOrders);
+      return formattedOrders; 
+    }
+
+    function findPizzasForOrder(orderId){
+      //helper function that will sort through the line_items table
+      //for any where the order_id matches our orderId parameter, 
+      //add it to an array pizzasIDsInThisOrder.
+      //so now we'll have the IDs but not the names of the pizzas. 
+      //for that we need to sort through the pizza table and find the matching IDs.
+    }
+
+    //need to repeat this ^ with pizzas, to get the pizzas from the db.
+    //compare the pizza ids in the orders table to the store.pizzas pizzas to get the actual names of the pies
 
     return (
-
         <TableContainer component={Paper}>
         <Table aria-label="collapsible table">
             <TableHead>
@@ -46,7 +77,7 @@ function DataTable() {
                 <TableCell/>
                 {/* ^ empty cell to hold place for arrows beneath, which require no title */}
                 <TableCell><h3>Name</h3></TableCell>
-                <TableCell align="right"><h3>Time Order Placed</h3></TableCell>
+                <TableCell align="right"><h3>{JSON.stringify(orders)} Time Order Placed</h3></TableCell>
                 <TableCell align="right"><h3>Type</h3></TableCell>
                 <TableCell align="right"><h3>Cost</h3></TableCell>
             </TableRow>
@@ -60,12 +91,12 @@ function DataTable() {
             </TableBody>
         </Table>
         </TableContainer>
-    );
+    ); 
 }
 
 function Row(props) {
-    //comes with MUI collapsible table template (altered)
-    //helper function that makes MUI rows out of our 'rows' variable
+    // comes with MUI collapsible table template (altered)
+    // helper function that makes MUI rows out of our 'rows' variable
 
     const { row } = props;
     const [open, setOpen] = React.useState(false);
--- src/index.js
+++ src/index.js
@@ -36,6 +36,13 @@ const orders =  (state = [], action) => {
     })
   }
 
+  if (action.type === 'SET_ORDERS'){
+    console.log('in index set orders');
+    return [...state, action.payload];
+  }
+
+
+  //v THIS IS GOING AWAY v
   if ( action.type === 'GET' ){
     axios.get('/api/order').then ( ( response )=>{
         console.log('In Get:', response.data);
@@ -44,20 +51,7 @@ const orders =  (state = [], action) => {
         console.log( err );
         alert( 'problem!' );
     }) 
-}
-//todo can remove 'annabel' and 'josie' placeholder/testing data once Admin successfully displays DB orders on DOM
-// { name: 'Annabel', 
-// orderTime: '11/13 at 7:13pm', 
-// type: 'delivery', 
-// cost: 27.99, 
-// pizzas: [ 
-//     {id: 1, quantity: 1}, 
-//     {id: 5, quantity: 1} 
-// ] },
-// { name: 'Josie', orderTime: '11/13 at 6:29pm', type: 'pickup', cost: 21.99, pizzas: [ {id: 2, quantity: 1}, {id: 3, quantity: 1}, {id: 4, quantity: 1}  ] }
-
-// ], action) => {
-// return state;
+  }
   return state;
 }
 
