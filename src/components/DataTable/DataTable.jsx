9ae90f8ba3c3c5dac60ccf1aadaa5026a44ffbb6
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-11-17 18:51:40 -0600

    finally displaying pizzas from each order! the solution (react alert) isn't the most obvious choice but that could be changed later if desired

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -15,14 +15,21 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import {useSelector, useDispatch} from 'react-redux';
 import {useEffect, useState} from 'react';
 import axios from 'axios';
+import { Button } from '@material-ui/core';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
+import Stack from '@mui/material/Stack';
 
-//DataTable is a sleek MUI collapsible table
+
+//DataTable is a sleek MUI table
 function DataTable() {
-    const orders = useSelector( store=>store.orders );
-   // const pizzas = useSelector( store=>store.pizzas );
-   // const lineItems = useSelector( store=>store.lineItems );
 
-    const [rows, setRows]=useState([]); //orders get stored as 'rows' for the purpose of rendering in the MUI components below
+    const orders = useSelector( store=>store.orders );
+
+    const [rows, setRows]=useState([]); //orders get stored as 'rows' for the purpose of mui stuff below
+    const [pizzasOnThisOrder, setPizzasOnThisOrder]=useState([]); 
+    const [show, setShow] = useState(false);
+
     const dispatch = useDispatch();
 
     useEffect(()=>{ 
@@ -60,57 +67,15 @@ function DataTable() {
     }
 
 
-    return (
-        <TableContainer component={Paper}>
-        <Table aria-label="collapsible table">
-            <TableHead>
-            <TableRow>
-
-             {/* Table TITLES */}
-                <TableCell/>
-                {/* ^ empty cell to hold place for arrows beneath, which require no title */}
-                <TableCell><h3>Name</h3></TableCell>
-                <TableCell align="right"><h3>Time Order Placed</h3></TableCell>
-                <TableCell align="right"><h3>Type</h3></TableCell>
-                <TableCell align="right"><h3>Cost</h3></TableCell>
-            </TableRow>
-            </TableHead>
-            <TableBody>
-            
-            {/* Table ROWS */}
-            {rows.map((row) => (
-                <Row key={row.name} row={row}/>
-            ))}
-            </TableBody>
-        </Table>
-        </TableContainer>
-    ); 
-}
-
-
-function Row(props) {
-    // comes with MUI collapsible table template (altered)
-    // helper function that makes MUI rows out of our 'rows' variable
-    const dispatch = useDispatch();
-
-    const { row } = props;
-    const [open, setOpen] = React.useState(false);
-
-    const dropdownClicked = ()=> {
-      setOpen(!open); //opens or closes the dropdown menu
-    }
-
-    const showOrderDetails = () => {
+    const showOrderDetails = (id) => {
       //function that takes in an order ID and makes an axios call to do some SQL joins, 
       //to ultimately display a list of pizzas belonging to each customer order
 
-      console.log("!", row.id);
-
       let thisOrdersPizzas = []
-        axios.get(`/api/lineItem/${row.id}`).then ( ( response )=>{
+        axios.get(`/api/lineItem/${id}`).then ( ( response )=>{
           dispatch({type: 'SET_LINE_ITEMS', payload: response.data});
-          
-          console.log("this order's pizzas:", response.data);
+          setPizzasOnThisOrder(response.data);
+          setShow(true);
         }).catch( ( err )=>{
           console.log( err );
         alert( 'problem!' );
@@ -118,68 +83,60 @@ function Row(props) {
 
     }
 
-    return (
-      <React.Fragment>
-        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-          <TableCell>
-            <IconButton
-              aria-label="expand row"
-              size="small"
-              onClick={dropdownClicked}
+  return (
+    <div>
+    
+      {show ? 
+      <Alert show={show} severity="info" align="center" dismissible>
+        <AlertTitle  align="left">This Customer's Order Details:</AlertTitle>
+        {pizzasOnThisOrder.map((pizza) => (
+              <p>{pizza.name}</p>
+            ))}
+        <div className="alert-close-button">
+            <Button onClick={() => setShow(false)} variant="outline-success">
+              Close
+            </Button>
+        </div> 
+      </Alert>
+      :
+      <p></p>
+      }
+      
+
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+             {/* Table TITLES */}
+              <TableCell><h3>Name</h3></TableCell>
+              <TableCell align="right"><h3>Time Order Placed</h3></TableCell>
+              <TableCell align="right"><h3>Type</h3></TableCell>
+              <TableCell align="right"><h3>Cost</h3></TableCell>
+              <TableCell/>
+              {/* ^ Empty cell to go above our buttons */}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-          </TableCell>
+              {/* Table CELLS that contain our data (passed from orders to rows) */}
+              <TableCell component="th" scope="row">{row.name}</TableCell>
+              <TableCell align="right">{row.orderTime}</TableCell>
+              <TableCell align="right">{row.type}</TableCell>
+              <TableCell align="right">{row.cost}</TableCell>
+              <TableCell align="right"><Button onClick={() => showOrderDetails(row.id)}>Details</Button></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
 
-          {/* Table CELLS that contain our data (passed from orders to rows) */}
-          <TableCell component="th" scope="row">{row.name}</TableCell>
-          <TableCell align="right">{row.orderTime}</TableCell>
-          <TableCell align="right">{row.type}</TableCell>
-          {/* <TableCell align="right">{row.cost}</TableCell> */}
-           <TableCell align="right"><button onClick={showOrderDetails}>Details</button></TableCell>
+    </div>
+  );
+}
 
-        </TableRow>
-        <TableRow>
-
-          {/* Collapsible portion - shows which pizzas were on each order   */}
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <Box sx={{ margin: 1 }}>
-                <h4 align="center">Pizzas Ordered</h4>
-                <Table size="small" aria-label="purchases">
-                  <TableBody>
-                    
-                    {/* Map through the pizza objects and display their ID and QTY */}
-                    {row.pizzas.map( (pizza) => (
-                    <TableRow key={row.id}>
-                        <TableCell align="center">ID:{pizza.id} | Quantity:{pizza.quantity}</TableCell>
-                    </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </Box>
-            </Collapse>
-          </TableCell>
-        </TableRow>
-      </React.Fragment>
-    );
-  }
-  
-  Row.propTypes = {
-    //comes with MUI collapsible table template (altered)
-    //helper function that formats the data for the table
-
-    row: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      orderTime: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      cost: PropTypes.string.isRequired,
-      pizzas: PropTypes.arrayOf(
-        PropTypes.shape({
-        }),
-      ).isRequired
-    }).isRequired,
-  };
-  
 
 export default DataTable;
